85aaa140abacab854e1205c90bfda0b82362920d
nghgeir <91161107+nghgeir@users.noreply.github.com>
2025-02-03 21:56:52 +0100

    fontfix
    fontfix

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,15 +2,15 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 const ibmPlexSans = IBM_Plex_Sans({
   subsets: ['latin'], // Customize subsets as needed
