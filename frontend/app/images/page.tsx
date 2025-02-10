5736e17e81c072250f320b380f38a51e42419cf7
nghgeir <91161107+nghgeir@users.noreply.github.com>
2025-02-10 21:46:40 +0100

    desfix
    desfix

--- frontend/app/images/page.tsx
+++ frontend/app/images/page.tsx
@@ -460,9 +460,9 @@ export default function Page() {
                 </div>
 
 
-                <div className="mx-auto space-y-8 px-2 lg:px-8 lg:py-8">
+                <div className="mx-auto px-2">
                     <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
-                        <div className="rounded-lg bg-black p-3.5 lg:p-6 w-full">
+                        <div className="rounded-lg bg-black w-full pt-2">
                             <div className="prose prose-sm prose-invert max-w-none">
                                 <div className="flex flex-wrap items-center justify-start gap-2">
                                     {imagesFolders?.map((folder) => {
