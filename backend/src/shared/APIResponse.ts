0af46b48a06a377d6f6ade0c4bb54748ca062508
nghgeir <91161107+nghgeir@users.noreply.github.com>
2025-03-28 10:59:23 +0100

    d
    d

--- backend/src/shared/APIResponse.ts
+++ backend/src/shared/APIResponse.ts
@@ -1,4 +1,5 @@
 export function APIResponse(status: string, code: number, message: string, details: string, id: number | null) {
     let json = JSON.stringify({ status, code, message, details, id });
+    console.log(json);
     return json;
 }
