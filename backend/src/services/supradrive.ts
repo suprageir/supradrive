9610aa5f961a2b3b5f0c6b463e38ab202623656b
nghgeir <91161107+nghgeir@users.noreply.github.com>
2025-03-28 10:50:56 +0100

    idfix
    idfix

--- backend/src/services/supradrive.ts
+++ backend/src/services/supradrive.ts
@@ -855,7 +855,7 @@ export abstract class sqlSupraDrive {
                         const query = `INSERT INTO videofile (videofolderid, videouserid, videosha1, videofilename, videofilenamedisk, videosize, videoformat, videoduration, videowidth, videoheight, videocodec, videodate, videotime, videometajson) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
                         const values = [folderid, userid, filesha1, filename, filenamedisk, filesize, videoMetadata.format, videoMetadata.duration, videoMetadata.width, videoMetadata.height, videoMetadata.codec, recordingDate, recordingTime, JSON.stringify(videoMetadata)];
                         const [result] = await supradrive.query(query, values);
-                        return APIResponse("success", 200, "Video " + filename + " uploaded successfully", "", result[0].insertId || null);
+                        return APIResponse("success", 200, "Video " + filename + " uploaded successfully", "", result.insertId);
                     } catch (e) {
                         console.log(e);
                     }
@@ -867,7 +867,7 @@ export abstract class sqlSupraDrive {
                         const query = `INSERT INTO videofile (videofolderid, videouserid, videosha1, videofilename, videofilenamedisk, videosize, videoformat, videoduration, videowidth, videoheight, videocodec, videodate, videotime, videometajson) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
                         const values = [folderid, userid, filesha1, filename, filenamedisk, filesize, videoMetadata.format, videoMetadata.duration, videoMetadata.width, videoMetadata.height, videoMetadata.codec, recordingDate, recordingTime, JSON.stringify(videoMetadata)];
                         const [result] = await supradrive.query(query, values);
-                        return APIResponse("success", 200, "Video " + filename + " uploaded successfully (meta error)", "", result[0].insertId || null);
+                        return APIResponse("success", 200, "Video " + filename + " uploaded successfully (meta error)", "", result.insertId);
 
                     } catch (e) {
                         console.log(e);
