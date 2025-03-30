e5811bc4e7fed8409d373f0da84e24475cc7a894
nghgeir <91161107+nghgeir@users.noreply.github.com>
2025-03-30 01:39:09 +0100

    ss
    ss

--- backend/src/routes/supradrive.ts
+++ backend/src/routes/supradrive.ts
@@ -117,8 +117,6 @@ export async function SupraDriveNewImagesUpload(req: MulterRequest, res: Respons
         console.log("\x1b[1m\x1b[30m[" + ts + "] [\x1b[32mOK\x1b[30m] [\x1b[35m" + username + "\x1b[30m] => \x1b[32mPOST\x1b[30m => \x1b[36m" + req.originalUrl);
         let posts: any = await sqlSupraDrive.SupraDriveNewImagesUpload(userid, username, req.body, req.file);
         let json = JSON.parse(posts);
-        console.log(json);
-        console.log(json.status);
         if (json.status === "success") {
             return res.status(OK).json(posts);
         }
